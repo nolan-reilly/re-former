3c93d78e55e6a821dfd97b890f4a23f22b950c42
Nolan Reilly <nolanreilly02@gmail.com>
2025-06-17 08:36:58 -0500

    Add edit page and error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    puts params
-
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     # @user = User.new(params[:user])
     @user = User.new(user_params)
@@ -14,6 +13,20 @@ class UsersController < ApplicationController
       redirect_to new_user_path
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to edit_user_path(@user)
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
